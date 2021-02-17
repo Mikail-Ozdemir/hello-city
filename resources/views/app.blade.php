9968c1b9718b3797750545e99a38f6df63befe85
ubuntu <professional.mikailozdemir@gmail.com>
2021-02-17 14:48:21 +0100

    Do not display footer about us link on about page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,8 +13,11 @@
     {{ config('database.connections.sqlite.driver') }}
 
     <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot;
-            <a href="{{ route('about') }}">About Us</a>
+        <p>&copy; Copyright {{ date('Y') }}
+
+            @if(! Route::is('about'))
+            &middot; <a href="{{ route('about') }}">About Us</a>
+            @endif
         </p>
     </footer>
 </body>
