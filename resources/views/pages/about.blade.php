e6f0679c5bbb85cb19388306f412f65c7d8755de
ubuntu <professional.mikailozdemir@gmail.com>
2021-02-19 12:49:21 +0100

    modif of flag picture

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,11 +6,16 @@
 
 
 @section('content')
-    <img src="{{ asset('/images/tdn-logo.png') }}" alt="TDN logo">
+    <img src="{{ asset('/images/tdn-logo.png') }}" alt="TDN logo" class="my-12 rounded-full shadow-md">
 
-    <p>Built with &hearts; by LES TEACHERS DU NET. </p>
+    <h2 class="mb-5 text-gray-700">
+        Built with <span class="text-pink-500">&hearts;</span>  by LES TEACHERS DU NET.
+    </h2>
 
-    <p><a href="{{'/'}}">Revenir à la page d'accueil</a></p>
+    <p>
+        <a href="{{ route('home') }}" class="text-indigo-500 hover:text-indigo-600 underline">
+            Revenir à la page d'accueil</a>
+    </p>
 
 
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,10 +5,11 @@
 
 
 @section('content')
-    <img src="{{ asset('/images/quebec-flag.png') }}" alt="Quebec Flag">
 
-        <h1>Hello from Quebec!</h1>
+        <img src="{{ asset('/images/quebec-flag.png') }}" alt="Quebec Flag" class="mt-12 rounded shadow-md h-32>
 
-        <p>It's currently {{ date('h:i A') }}.</p>
+        <h1 class="mt-5 text-3xl sm:text-5xl font-semibold text-indigo-600">Hello from Quebec!</h1>
+
+        <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }}.</p>
 
 @endsection
